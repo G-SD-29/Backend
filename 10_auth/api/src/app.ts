fbfecf5a95383569aa2c96e180b385a624290b13
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-09-07 17:00:10 +0200

    add starter repo for auth

---
+++ 10_auth/api/src/app.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express from 'express';
+import '#db';
+import { errorHandler, notFoundHandler } from '#middleware';
+import { postRoutes, authRoutes } from '#routes';
+import { PORT, CLIENT_BASE_URL } from '#config';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: CLIENT_BASE_URL, // for use with credentials, origin(s) need to be specified
+    credentials: true, // sends and receives secure cookies
+    exposedHeaders: ['WWW-Authenticate'] // needed to send the 'refresh trigger''
+  })
+);
+app.use(express.json());
+
+app.use('/posts', postRoutes);
+app.use('/auth', authRoutes);
+
+app.use('*splat', notFoundHandler);
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`API Server listening on http://localhost:${PORT}`));
